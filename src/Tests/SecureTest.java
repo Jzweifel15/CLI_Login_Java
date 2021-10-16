2177573b91205265c805f2614f0689ae095dd8cb
Justin R. Zweifel <jzweifel15@yahoo.com>
2021-10-16 12:18:33 -0400

    Added Extra Tests
    extra test passwords were added to ensure validity

--- src/Tests/SecureTest.java
+++ src/Tests/SecureTest.java
@@ -7,30 +7,41 @@ import org.junit.After;
 import org.junit.Test;
 
 public class SecureTest {
-    String testPassword;
+    String testPassword1;
+    String testPassword2;
+    String testPassword3;
 
     // Pre-test setup
     @Before
     public void pretest() {
-        testPassword = "12+HelloWorld+21";
+        testPassword1 = "12+HelloWorld+21";
+        testPassword2 = "<!TestDrivenDev342";
+        testPassword3 = "*Java_Development47*";
     }
 
     // Test Encryption
     @Test
     public void testEncryption() {
-        Assert.assertTrue("Test encryption did not match", Secure.encrypt(testPassword).equals("97_PslliAiflt_79"));
+        Assert.assertTrue("Test encryption for testPassword1 did not match", Secure.encrypt(testPassword1).equals("97_PslliAiflt_79"));
+        Assert.assertTrue("Test encryption for testPassword2 did not match", Secure.encrypt(testPassword2).equals("?]DsedTfobsjTsb567"));
+        Assert.assertTrue("Test encryption for testPassword3 did not match", Secure.encrypt(testPassword3).equals("(Nwbw+Tsbslihksjd62("));
     }
 
     // Test Decryption
     @Test
     public void testDecryption() {
-        Assert.assertTrue("Test decryption did not match", Secure.decrypt("97_PslliAiflt_79").equals(testPassword));
+        // TODO: Add the proper decryption sequence for testPasswords 2 and 3
+        Assert.assertTrue("Test decryption for testPassword1 did not match", Secure.decrypt("97_PslliAiflt_79").equals(testPassword1));
+        Assert.assertTrue("Test decryption for testPassword2 did not match", Secure.decrypt("?]DsedTfobsjTsb567").equals(testPassword2));
+        Assert.assertTrue("Test decryption for testPassword3 did not match", Secure.decrypt("(Nwbw+Tsbslihksjd62(").equals(testPassword3));
     }
 
     // Post-test teardown
     @After
     public void postTest() {
-        testPassword = "";
+        testPassword1 = "";
+        testPassword2 = "";
+        testPassword3 = "";
     }
 
 }
