b138d5794145b74145b7523eecd6fa19e37b7d04
Justin R. Zweifel <jzweifel15@yahoo.com>
2021-10-16 11:00:08 -0400

    Update SecureTest.java
    Added pre-test set up and a single test for testing the Secure.encrypt() method

--- src/Tests/SecureTest.java
+++ src/Tests/SecureTest.java
@@ -1,10 +1,30 @@
 package Tests;
 
+import Security.Secure;
 import org.junit.Assert;
 import org.junit.Before;
-//import org.junit.After;
+import org.junit.After;
 import org.junit.Test;
 
 public class SecureTest {
-    // TODO: Add Testing for Secure class
+    String testPassword;
+
+    // Pre-test setup
+    @Before
+    public void pretest() {
+        testPassword = "12+HelloWorld+21";
+    }
+
+    // Test Encryption
+    @Test
+    public void testEncryption() {
+        Assert.assertTrue("Test encryption did not match", Secure.encrypt(testPassword).equals("97_PslliAiflt_79"));
+    }
+
+    // Post-test teardown
+    @After
+    public void postTest() {
+        testPassword = "";
+    }
+
 }
